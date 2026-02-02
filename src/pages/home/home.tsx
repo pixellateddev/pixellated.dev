69da73c079b70af5371c5923c50c3b9fc62c52f7
Himanshu Sagar <pixellateddev@proton.me>
2026-02-02 19:26:38 +0530

    fixed biome

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import type { FC } from 'react'
+
+const Home: FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Pixellated!</h1>
+      <p>This is the home page.</p>
+    </div>
+  )
+}
+
+export default Home
