6ff15c7d6085934215b8678158051384a1cab1ce
Himanshu Sagar <pixellateddev@proton.me>
2026-02-02 08:21:40 +0530

    testing node runtime

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ const config = defineConfig({
   },
   plugins: [
     devtools(),
-    nitro({ preset: "bun" }),
+    nitro(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
