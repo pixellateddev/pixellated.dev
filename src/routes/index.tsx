9445ac38212f598d381c3c618d502d321afaf108
Himanshu Sagar <pixellateddev@proton.me>
2026-02-02 08:51:15 +0530

    added coming soon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,5 +3,12 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-  return <div>Welcome</div>;
+  return (
+    <div className="container">
+      <h1>
+        Welcome to <span>Pixellated.dev</span>
+      </h1>
+      <h2>Coming Soon...</h2>
+    </div>
+  );
 }
